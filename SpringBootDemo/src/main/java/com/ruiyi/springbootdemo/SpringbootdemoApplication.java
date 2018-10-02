4ee5cef52fcf0791a7235ea7a448c099a33700d6
胡睿毅 <807776962@qq.com>
2018-10-02 22:49:27 +0800

    SpringBootDemo

---
+++ SpringBootDemo/src/main/java/com/ruiyi/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,12 @@
+package com.ruiyi.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoApplication.class, args);
+    }
+}
