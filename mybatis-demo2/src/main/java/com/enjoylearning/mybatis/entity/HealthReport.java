30e2b2ea142ff27d08fa4e180ca0af3773f3753b
胡睿毅 <807776962@qq.com>
2018-11-14 16:38:38 +0800

    Clear

---
+++ mybatis-demo2/src/main/java/com/enjoylearning/mybatis/entity/HealthReport.java
@@ -0,0 +1,15 @@
+package com.enjoylearning.mybatis.entity;
+
+public class HealthReport {
+
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
--- mybatis-quick-start/src/main/java/com/enjoylearning/mybatis/basic/mapper/TUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.enjoylearning.mybatis.basic.mapper;
-
-import com.enjoylearning.mybatis.basic.entity.TUser;
-
-public interface TUserMapper {
-
-	TUser selectByPrimaryKey(Integer id);
-
-}
