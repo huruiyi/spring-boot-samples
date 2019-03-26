649850b3eef9fa807baf6675e524f718fd77e3d6
胡睿毅 <807776962@qq.com>
2019-03-26 16:12:00 +0800

    idea git test

--- JavaDemo/src/main/java/com/example/io.java
+++ JavaDemo/src/main/java/com/example/io.java
@@ -6,11 +6,6 @@ import java.util.Properties;
 
 public class io {
     public static void main(String[] args) {
-
-        // 分支测试
-        System.out.println("1111111111111111111111111111111111111111");
-        System.out.println("1111111111111111111111111111111111111111");
-
         Properties properties = System.getProperties();
         System.out.println(properties.size());
         Enumeration<Object> keys = properties.keys();
@@ -19,7 +14,6 @@ public class io {
             Object key = keys.nextElement();
             Object value = properties.get(key);
             System.out.println(++i + ":" + key + ":" + value);
-
         }
     }
 }
