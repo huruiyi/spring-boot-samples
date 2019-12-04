4ca4020d5af4c339b38105e2e5dcc36517272aba
胡睿毅 <807776962@qq.com>
2019-12-04 09:51:40 +0800

    Spring-Boot-Redis-Sessio

---
+++ SpringBootRedisSession/src/test/java/com/example/rsession/SpringBootRedisSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedisSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
