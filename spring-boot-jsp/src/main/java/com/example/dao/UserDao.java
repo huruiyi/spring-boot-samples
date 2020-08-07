7d4f22c37bf5f67881798b290b48fbfb7ea96ce4
胡睿毅 <807776962@qq.com>
2020-08-07 22:00:13 +0800

    remove spirng boot jsp

--- spring-boot-jsp/src/main/java/com/example/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.dao;
-
-import com.example.model.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserDao {
-    public User getNameById(User user);
-}
