8c584c70a49d0993af3a1d33f575410fa3b1b328
胡睿毅 <807776962@qq.com>
2022-01-03 19:24:06 +0800

    spring-boot-swagger-ui

---
+++ spring-boot-swagger-ui/src/main/java/com/example/demo/SpringBootSwaggerUiApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwaggerUiApplication {
+
+    // swagger-ui:
+    //  http://localhost:8080/swagger-ui.html
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSwaggerUiApplication.class, args);
+    }
+
+}
---
+++ spring-boot-swagger-ui/src/main/java/com/example/demo/web/UserController.java
@@ -0,0 +1,40 @@
+package com.example.demo.web;
+
+import com.example.demo.model.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Api(tags = "用户操作api")
+public class UserController {
+
+    @PutMapping("user")
+    @ApiOperation(value = "添加用户")
+    @ApiImplicitParam(name = "user", value = "用户实体类", dataType = "User")
+    public String addUser(@RequestBody User user) {
+        return "Hello World";
+    }
+
+    @PostMapping("user")
+    @ApiOperation(value = "根据id更新用户信息")
+    @ApiImplicitParam(name = "user", value = "用户实体类", dataType = "User")
+    public String updateUser(@RequestBody User user) {
+        return "Hello World";
+    }
+
+    @DeleteMapping("user/{uid}")
+    @ApiOperation(value = "根据id删除用户")
+    @ApiImplicitParam(name = "uid", value = "用户id", dataType = "Integer")
+    public String deleteUserById(@PathVariable Integer uid) {
+        return "Hello World";
+    }
+
+    @ApiOperation(value = "根据用户id查询用户记录")
+    @ApiImplicitParam(name = "uid", value = "查询参数", required = true)
+    @GetMapping("user/{uid}")
+    public String findUserById(@PathVariable Integer uid) {
+        return "Hello World";
+    }
+}
