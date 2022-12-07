80461d775318c0ad49e41c91b6ea35e948ca3355
胡睿毅 <807776962@qq.com>
2022-12-08 06:25:47 +0800

    rename

--- other-samples/pro-spring-5-ch03/dependency-pull/src/main/java/com/apress/prospring5/ch3/DependencyPull.java
+++ other-samples/pro-spring-5-ch03/dependency-pull/src/main/java/com/apress/prospring5/ch3/DependencyPull.java
@@ -7,7 +7,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class DependencyPull {
     public static void main(String... args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/app-context.xml");
-
         MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
         mr.render();
     }
--- spring-boot/src/main/java/com/example/Spring/config/JavaConfig.java
+++ spring-boot/src/main/java/com/example/Spring/config/JavaConfig.java
@@ -2,12 +2,14 @@ package com.example.Spring.config;
 
 import com.example.Spring.model.Person;
 import com.example.Spring.service.unclassified.SingleService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableScheduling
@@ -36,4 +38,32 @@ public class JavaConfig {
         return  mailSender;
     }
 
+    @Bean
+    public CorsFilter  corsWebFilter() {
+        //1.添加CORS配置信息
+        CorsConfiguration config = new CorsConfiguration();
+        //1) 允许的域,不要写*，否则cookie就无法使用了
+        config.addAllowedOrigin("http://localhost:8080");
+        //2) 是否发送Cookie信息
+        config.setAllowCredentials(true);
+        //3) 允许的请求方式
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        // 4）允许的头信息
+        config.addAllowedHeader("*");
+        // 4）有效时间
+        config.setMaxAge(3600L);
+
+        //2.添加映射路径，我们拦截一切请求
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", config);
+
+        //3.返回新的CorsFilter.
+        return new CorsFilter(configSource);
+    }
 }
