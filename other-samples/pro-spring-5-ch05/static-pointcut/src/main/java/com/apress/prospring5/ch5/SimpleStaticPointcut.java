8509aaf065d7ab750c8fa809c97e9a0efc4f5815
胡睿毅 <807776962@qq.com>
2023-03-16 15:46:34 +0800

    spring ch05

---
+++ other-samples/pro-spring-5-ch05/static-pointcut/src/main/java/com/apress/prospring5/ch5/SimpleStaticPointcut.java
@@ -0,0 +1,18 @@
+package com.apress.prospring5.ch5;
+
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.support.StaticMethodMatcherPointcut;
+
+import java.lang.reflect.Method;
+
+public class SimpleStaticPointcut extends StaticMethodMatcherPointcut {
+    @Override
+    public boolean matches(Method method, Class<?> cls) {
+        return ("sing".equals(method.getName()));
+    }
+
+    @Override
+    public ClassFilter getClassFilter() {
+        return cls -> (cls == GoodGuitarist.class);
+    }
+}
---
+++ other-samples/pro-spring-5-ch05/static-pointcut/src/main/java/com/apress/prospring5/ch5/common/Singer.java
@@ -0,0 +1,5 @@
+package com.apress.prospring5.ch5.common;
+
+public interface Singer {
+	void sing();
+}
