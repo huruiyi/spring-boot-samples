f9d1138cf3b9a083d07b3e87816edbbe9b1a41b3
胡睿毅 <807776962@qq.com>
2023-08-30 16:56:03 +0800

    Date JsonFormat

---
+++ spring-boot/src/main/java/com/example/model/SampleDto.java
@@ -0,0 +1,32 @@
+package com.example.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class SampleDto {
+
+  /**
+   * <a href="https://tc39.es/ecma262/">...</a>
+   * <a href="https://262.ecma-international.org/14.0/">...</a>
+   * <a href="https://262.ecma-international.org/6.0/#sec-date-time-string-format%C2%A0">...</a>
+   */
+  public static final String DATE_FORMAT_ISO8601 =  "YYYY-MM-DDTHH:mm:ss.sssZ";
+
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX", timezone = "Asia/Kolkata")
+  private Instant instant;
+
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+  private Date date;
+
+  @JsonFormat(pattern = "yyyy-MM-dd")
+  private LocalDate localDate;
+
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+  private LocalDateTime localDateTime;
+
+}
---
+++ spring-boot/src/main/java/com/example/web/DateController.java
@@ -0,0 +1,31 @@
+package com.example.web;
+
+import com.example.model.SampleDto;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Date;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DateController {
+
+  @PostMapping("/date")
+  public SampleDto create(@RequestBody SampleDto payload) {
+    return payload;
+  }
+
+  @GetMapping("/date")
+  public SampleDto get() {
+    final SampleDto dto = new SampleDto();
+    final Instant time = Instant.ofEpochMilli(1571884105000L);
+    dto.setInstant(time);
+    dto.setDate(new Date(time.toEpochMilli()));
+    dto.setLocalDate(time.atZone(ZoneId.of("UTC")).toLocalDate());
+    dto.setLocalDateTime(time.atZone(ZoneId.of("UTC")).toLocalDateTime());
+    return dto;
+  }
+
+}
