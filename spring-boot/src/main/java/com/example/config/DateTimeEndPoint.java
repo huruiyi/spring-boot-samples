1c6c763d63e48e55b6633a96c3ca9d588db48bff
胡睿毅 <807776962@qq.com>
2023-11-21 13:51:02 +0800

    重构

--- spring-boot/src/main/java/com/example/config/DateTimeEndPoint.java
+++ spring-boot/src/main/java/com/example/config/DateTimeEndPoint.java
@@ -13,11 +13,6 @@ public class DateTimeEndPoint {
 
   private String format = "yyyy-MM-dd HH:mm:ss";
 
-  /**
-   * http://localhost:9000/actuator/datetime 用来显示监控指标
-   *
-   * @return map
-   */
   @ReadOperation
   public Map<String, Object> info() {
     HashMap<String, Object> map = new HashMap<>();
@@ -25,13 +20,6 @@ public class DateTimeEndPoint {
     return map;
   }
 
-  /**
-   * 动态更改监控指标:http://localhost:9000/actuator/datetime
-   * Post:
-   * {
-   *     "format": "日期：yyyy-MM-dd 时间：HH:mm:ss)"
-   * }
-   */
   @WriteOperation
   public void setDateTime(String format) {
     this.format = format;
