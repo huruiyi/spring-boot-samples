b880f2b8cd9274362cd6a500ecaee08b82691ea0
胡睿毅 <807776962@qq.com>
2023-11-21 16:58:30 +0800

    oauth2-redis-server

--- spring-boot-oauth2/oauth2-redis-server/src/main/java/com/example/RedisServerApplication.java
+++ spring-boot-oauth2/oauth2-redis-server/src/main/java/com/example/RedisServerApplication.java
@@ -7,10 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RedisServerApplication {
 
   /**
-   * http://localhost:8080/oauth/token?username=user_1&password=123456&grant_type=password&scope=select&client_id=client_2&client_secret=123456
-   * http://localhost:8080/hello http://localhost:8080/api/profile?access_token=obs-QIO21wezfsNWi1rywpbo4lw
-   *
-   * @param args
+   * get access_token:
+   * client_1:
+   * http://localhost:20003/oauth/token?username=user_1&password=123456&grant_type=client_credentials&scope=select&client_id=client_1&client_secret=123456
+   * client_2:
+   * http://localhost:20003/oauth/token?username=user_1&password=123456&grant_type=password&scope=select&client_id=client_2&client_secret=123456
+   * <p>
+   * http://localhost:20003/hello
+   * <p>
+   * use access_token:
+   * http://localhost:20003/api/profile?access_token=obs-QIO21wezfsNWi1rywpbo4lw
    */
   public static void main(String[] args) {
     SpringApplication.run(RedisServerApplication.class, args);
---
+++ spring-boot-oauth2/oauth2-redis-server/src/main/java/com/example/consts/ResourceIds.java
@@ -0,0 +1,7 @@
+package com.example.consts;
+
+public class ResourceIds {
+
+  public static final String DEMO_RESOURCE_ID = "api";
+
+}
